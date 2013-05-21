3ca9077e1fcea08d6306aaad94e99859fcf9d50e
krahim <karim.rahim@gmail.com>
2013-05-21 19:31:13 -0400

    clean up file ;;

--- R/harmonic.R
+++ R/harmonic.R
@@ -21,7 +21,7 @@
 ## ################################################################################
 
 FishergStat <- function(timeSeries,
-                     centreData=T,
+                     centreData=TRUE,
                      alpha=.05) {
     
     ##         "given
@@ -47,47 +47,47 @@ FishergStat <- function(timeSeries,
     ##        of white noise
     ## ---
     ## Note: see Section 10.9 of the SAPA book"
-    result <- NULL;
-    N <- length(timeSeries);
-    M <- length(timeSeries);
-    m <- 0;
+    result <- NULL 
+    N <- length(timeSeries) 
+    M <- length(timeSeries) 
+    m <- 0 
     if(N%%2==0) {
-        m <- (N-2)/2;
+        m <- (N-2)/2 
     } else {
-        m <- (N-1)/2;
+        m <- (N-1)/2 
     }
     
     if(m<= 1) {
-        return(list(gStat=1.0, critical=1.0, hyp="Fail To Reject"));
+        return(list(gStat=1.0, critical=1.0, hyp="Fail To Reject")) 
     }
-    gF <- 1.0 - ( (alpha/m)^(1/(m-1)));
+    gF <- 1.0 - ( (alpha/m)^(1/(m-1))) 
     thePeriodogram <- periodogram(timeSeries,
                                   centreData,
                                   nNonZeroFreqs="Fourier",
-                                  returnEstFor0FreqP=F,
-                                  sdfTransformation=F);
-    maxShp <- max(thePeriodogram$sdf[1:m]);
-    sumShp <- sum(thePeriodogram$sdf[1:m]);
-    gStatistic <- maxShp/as.double(sumShp);
+                                  returnEstFor0FreqP=FALSE,
+                                  sdfTransformation=FALSE) 
+    maxShp <- max(thePeriodogram$sdf[1:m]) 
+    sumShp <- sum(thePeriodogram$sdf[1:m]) 
+    gStatistic <- maxShp/as.double(sumShp) 
     if(gStatistic > gF) {
-        result <- "Reject";
+        result <- "Reject" 
     } else {
-        result <- "Fail to Reject";
+        result <- "Fail to Reject" 
     }
     return(list(gStatistic=gStatistic,
                 gF=gF,
-                result=result));
+                result=result)) 
 }
 
 
 periodogramAtOneFrequency <- function(timeSeries, 
                        freq, 
-                       centreData=T,
+                       centreData=TRUE,
                        samplingTime=1.0) {
     ## #   "given
     ## #    [1] time-series (required)
     ## #        ==> a vector of time series values
-    ## #    [2] center-data (keyword; t)
+    ## #    [2] center-data (keyword;  t)
     ## #        ==> if t, subtract sample mean from time series;
     ## #            if a number, subtracts that number from time series;
     ## #            if nil, time-series is not centered
@@ -107,27 +107,30 @@ periodogramAtOneFrequency <- function(timeSeries,
     ## # ---
     ## # Note: see Section 10.2 of the SAPA book"
     
-   N <- length(timeSeries);
-   j <- 0;
+   N <- length(timeSeries)
+   j <- 0
    centreFactor <- 
-      if(is.numeric(centreData)) centreData 
-         else if(centreData) mean(centreData)
-         else 0;
-   cosSum <- 0;
-   sinSum <- 0;
-   twoPiFDeltT <- 2*pi*freq*samplingTime;
-   tIndex <- 1;
+      if(is.numeric(centreData))
+          centreData 
+      else if(centreData)
+          mean(centreData)
+      else
+          0
+   cosSum <- 0
+   sinSum <- 0
+   twoPiFDeltT <- 2*pi*freq*samplingTime
+   tIndex <- 1
    for(i in 0:(N -1)) {
       cosSum <- cosSum + (timeSeries[j +1] - centreFactor) *
-         cos(twoPiFDeltT*tIndex);
+         cos(twoPiFDeltT*tIndex)
       sinSum <- sinSum + (timeSeries[j +1] - centreFactor) *
-         sin(twoPiFDeltT*tIndex);
-      j <- j +1;
-      tIndex <- tIndex +1;   
+         sin(twoPiFDeltT*tIndex)
+      j <- j +1
+      tIndex <- tIndex +1   
    }
    return(list(valueAtFreq=(samplingTime/N)*(cosSum^2+sinSum^2),
       AEst=(2.0/N)*cosSum,
-      BEst=(2.0/N)*sinSum));
+      BEst=(2.0/N)*sinSum))
 }
 ##
 ## #X <- c(71.0,  63.0,  70.0,  88.0,  99.0,  90.0, 110.0, 135.0, 128.0, 154.0, 156.0, 141.0, 131.0, 132.0, 141.0, 104.0, 136.0, 146.0, 124.0, 129.0)
